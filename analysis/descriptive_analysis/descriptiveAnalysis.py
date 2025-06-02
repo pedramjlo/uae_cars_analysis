418d6a2e06b39ce102378a698081c30425a769e2
pedramjlo <pedram_sif@tuta.io>
2025-06-02 12:40:39 +0330

    added top 10 most expensive cars

--- analysis/descriptive_analysis/descriptiveAnalysis.py
+++ analysis/descriptive_analysis/descriptiveAnalysis.py
@@ -139,7 +139,41 @@ class VehicleAnalysis:
     def __init__(self, df, plot):
         self.df = df
         self.plot = plot
-        
+
+
+    def top_10_most_expensive_cars(self):
+        try:
+            
+            self.df["Vehicle"] = (
+                self.df["make"] + " " +
+                self.df["model"] + " " +
+                self.df["year"].astype(str)
+            )
+            
+
+            # Sort by price and take the top 10 rows directly
+            results = (
+                self.df.sort_values(by="price", ascending=False)
+                    .head(10)[["Vehicle", "price"]]
+            )
+
+            self.plot.bar_chart(
+                data=results,
+                x="Vehicle",
+                y="price",
+                labels={
+                    "model": "Vehicle",
+                    "price": "Price (AED)"
+                },
+                title="Top 10 Most Expensive Cars"
+            )
+
+            return results
+        except Exception as e:
+            raise e
+
+
+            
 
 
 
--- analysis/descriptive_analysis/run_descriptive_analysis.py
+++ analysis/descriptive_analysis/run_descriptive_analysis.py
@@ -7,6 +7,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"
 from data_visualisation.dataVisualisation import Visualisation
 
 from descriptiveAnalysis import BrandAnalysis
+from  descriptiveAnalysis import VehicleAnalysis
+
 import pandas as pd
 
 
@@ -15,10 +17,19 @@ if __name__ == "__main__":
     df = pd.read_csv("dataset/cleaned/cleaned_uae_cars.csv")
 
     visualisor = Visualisation()
+
     ba = BrandAnalysis(df=df, plot=visualisor)
+    va = VehicleAnalysis(df=df, plot=visualisor)
+
+
+    # BRAND ANALYSIS
     #ba.top_10_profitable_brands()
     #ba.least_10_profitable_brands()
-    #ba.most_expensive_brands()
+    #ba.top_10_expensive_brands()
     #ba.least_expensive_brands()
     #ba.top_10_eco_friendly_brands()
     #ba.fuel_type_market_share_by_brand()
+
+
+    # VEHICLE ANALYSIS
+    va.top_10_most_expensive_cars()
