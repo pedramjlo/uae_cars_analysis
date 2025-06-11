f50b9c41caa95392ce51525aecab57d2bb598900
pedramjlo <pedram_sif@tuta.io>
2025-06-11 20:48:06 +0330

    created data scaler constructor

--- data_scaling/dataScaling.py
+++ data_scaling/dataScaling.py
@@ -1,6 +1,15 @@
 from sklearn.preprocessing import StandardScaler
 
 
-class DataScaling:
-    def __init__(self, df):
+class DataScaler:
+    def __init__(self, df, method):
+        """
+        Args:
+            method (str): Scaling strategy. Supported: 
+                - "standard"     → zero mean, unit variance 
+                - "minmax"       → rescale to [0,1]
+                - "robust"       → use IQR for scaling (less sensitive to outliers)
+        """
         self.df = df
+        self.method = method
+        self.scaler = None
