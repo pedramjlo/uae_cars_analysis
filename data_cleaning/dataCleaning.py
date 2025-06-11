e9603e043321bc46a75d12441284e6f6564ea0d4
pedramjlo <pedram_sif@tuta.io>
2025-06-11 13:34:24 +0330

    moved create_condition_column from cleaning to FE

--- data_cleaning/dataCleaning.py
+++ data_cleaning/dataCleaning.py
@@ -116,27 +116,7 @@ class DataCleaner:
         return self
     
 
-    """
-    Extracting condition data from dscription column into a new column based on a regex pattern
-    """
-
-    def create_condition_column(self):
-        column_name = "condition"
-
-        if "description" in self.raw_dataset.columns:
-            if column_name not in self.raw_dataset.columns:
-                try:
-                    # Match city only if it appears after a comma at the end of the string
-                    regex_pattern = r'(?i)condition:\s*(.*)\.'
-                    self.raw_dataset[column_name] = self.raw_dataset["description"].str.extract(regex_pattern, flags=re.IGNORECASE)
-                    logging.info(f"Extracted city names to column {column_name}.")
-                except Exception as e:
-                    logging.error(f"Failed to extract the condition: {e}")
-            else:
-                logging.warning(f"Column {column_name} already exists!")
-        else:
-            logging.error("Column 'description' does not exist.")
-
+    
     
 
     def clean_all(self):
@@ -148,7 +128,6 @@ class DataCleaner:
         self.add_underscore_to_columns()
         self.imputate_null_values()
         self.remove_duplicates()
-        self.create_condition_column()
         return self.raw_dataset
 
     
--- feature_engineering/featureEngineering.py
+++ feature_engineering/featureEngineering.py
@@ -1,3 +1,14 @@
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,  # Set the minimum log level
+    format='%(asctime)s - %(levelname)s - %(message)s',  # Log message format
+    # filename='app.log'  # Log to a file (optional)
+)
+
+
+
 class FeatureEngineering:
     def __init__(self, df):
         self.df = df
@@ -5,3 +16,23 @@ class FeatureEngineering:
 
 
 
+    """
+    Extracting condition data from dscription column into a new column based on a regex pattern
+    """
+
+    def create_condition_column(self):
+        column_name = "condition"
+
+        if "description" in self.raw_dataset.columns:
+            if column_name not in self.raw_dataset.columns:
+                try:
+                    # Match city only if it appears after a comma at the end of the string
+                    regex_pattern = r'(?i)condition:\s*(.*)\.'
+                    self.raw_dataset[column_name] = self.raw_dataset["description"].str.extract(regex_pattern, flags=re.IGNORECASE)
+                    logging.info(f"Extracted city names to column {column_name}.")
+                except Exception as e:
+                    logging.error(f"Failed to extract the condition: {e}")
+            else:
+                logging.warning(f"Column {column_name} already exists!")
+        else:
+            logging.error("Column 'description' does not exist.")
