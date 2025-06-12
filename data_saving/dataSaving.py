c155f263218af81069346543554edf91230132de
pedramjlo <pedram_sif@tuta.io>
2025-06-12 16:57:17 +0330

    file path in data saving is now global variable

--- data_saving/dataSaving.py
+++ data_saving/dataSaving.py
@@ -3,23 +3,23 @@ import logging
 
 
 class DataSaver:
-
+    df_path='dataset/cleaned/cleaned_uae_cars.csv' 
 
     @staticmethod
-    def save_to_csv(df, df_path, index=False):
+    def save_to_csv(df, index=False):
         try:
-            df.to_csv(df_path, index=index)
-            logging.info(f"Data successfully saved to {df_path}")
+            df.to_csv(index=index)
+            logging.info(f"Data successfully saved to {DataSaver.df_path}")
         except Exception as e:
-            logging.error(f"Failed to save data to {df_path}: {e}")
+            logging.error(f"Failed to save data to {DataSaver.df_path}: {e}")
         return df
     
     
 
     @staticmethod
-    def save_cleaning_changes(df, df_path='dataset/cleaned/cleaned_uae_cars.csv'):
+    def save_cleaning_changes(df):
         try:
-            df.to_csv(df_path, index=False)
+            df.to_csv(DataSaver.df_path, index=False)
             logging.info("The cleaned dataset was successfully saved")
         except Exception as e:
             logging.error(f"Failed to save the cleaned data file: {e}")
